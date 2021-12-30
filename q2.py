a081c79774d1ec188bf9b0a7a0c67bfa0c917a2c
Fitsum Sileshi <fitsesile@gmail.com>
2021-12-30 17:38:47 +0300

    Add files via upload

---
+++ q2.py
@@ -0,0 +1,12 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+n=4
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    for j in range(2*(n-i-1)):
+        print(" ",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
